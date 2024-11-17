d288ffc09ef124b2ab3df0221f146433e997a6fe
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-11-18 01:08:01 +0300

    add: constraints

--- 10_DDL/02_constraints.sql
+++ 10_DDL/02_constraints.sql
@@ -31,4 +31,184 @@
 
 
 
+-- ====== FOREIGN KEY =======
+
+--CREATE TABLE roles (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(50) NOT NULL
+--);
+
+--CREATE TABLE users (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	email varchar(50) UNIQUE NOT NULL,
+--	role_id int NOT NULL,
+
+--	CONSTRAINT FK_users_roles FOREIGN KEY (role_id) REFERENCES roles(id)
+--);
+
+--INSERT INTO roles (title)
+--VALUES
+--	('admin'),
+--	('guest'),
+--	('author');
+
+--INSERT INTO users (email, role_id)
+--VALUES
+--	('vasia1@mail.com', 1),
+--	('vasia2@mail.com', 2),
+--	('vasia3@mail.com', 2);
+
+--INSERT INTO users (email, role_id)
+--VALUES ('vasia4@mail.com', 10);
+
+
+
+
+
+
+--CREATE TABLE roles (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(50) NOT NULL
+--);
+
+--CREATE TABLE users (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	email varchar(50) UNIQUE NOT NULL,
+--	role_id int NOT NULL DEFAULT(4),
+
+--	CONSTRAINT FK_users_roles FOREIGN KEY (role_id) REFERENCES roles(id)
+--		-- ON DELETE NO ACTION			-- по-умолчанию
+--		-- ON DELETE CASCADE			-- каскадное удаление (ОСТОРОЖНО!!!)
+--		-- ON DELETE SET NULL			-- установить NULL для поля внешнего ключа
+--		ON DELETE SET DEFAULT			-- установить значение по-умолчанию
+
+--		-- ON UPDATE NO ACTION			-- по-умолчанию
+--		-- ON UPDATE CASCADE			-- каскадное удаление (ОСТОРОЖНО!!!)
+--		-- ON UPDATE SET NULL			-- установить NULL для поля внешнего ключа
+--		-- ON UPDATE SET DEFAULT		-- установить значение по-умолчанию
+--);
+
+--INSERT INTO roles (title)
+--VALUES
+--	('admin'),
+--	('guest'),
+--	('author'),
+--	('default');
+
+--INSERT INTO users (email, role_id)
+--VALUES
+--	('vasia1@mail.com', 1),
+--	('vasia2@mail.com', 2),
+--	('vasia3@mail.com', 2);
+
+--DELETE FROM roles WHERE id = 1;
+
+--DROP TABLE users;
+--DROP TABLE roles;
+
+
+
+
+-- ====== NOT NULL =======
+--CREATE TABLE permissions (
+--	id int PRIMARY KEY IDENTITY(1,1)		NOT NULL,
+--	title varchar(50)						NOT NULL,
+--	description varchar(256)				NULL
+--);
+
+--INSERT INTO permissions(title, description)
+--VALUES
+--	('read', 'read the data'),
+--	('write', NULL);
+
+--UPDATE permissions
+--SET description = 'no_description'
+--WHERE description IS NULL;
+
+---- добавление
+--ALTER TABLE permissions
+--ALTER COLUMN description varchar(256) NOT NULL
+
+---- удаление
+--ALTER TABLE permissions
+--ALTER COLUMN description varchar(256) NULL
+
+--DROP TABLE permissions;
+
+
+
+
+-- ====== UNIQUE =======
+
+--CREATE TABLE roles (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(50) NOT NULL
+--);
+
+--CREATE TABLE users (
+--	id int IDENTITY(1,1)	NOT NULL,
+--	email varchar(50)		NOT NULL,
+--	username varchar(50)	NOT NULL,
+--	role_id int				NOT NULL,
+
+--	CONSTRAINT PK_users_id PRIMARY KEY(id),
+--	CONSTRAINT UQ_users_email UNIQUE(email),
+--	CONSTRAINT FK_users_role FOREIGN KEY (role_id) REFERENCES roles(id)
+--);
+
+---- добавление
+--ALTER TABLE users
+--ADD CONSTRAINT UQ_users_username UNIQUE(username)
+
+---- удаление
+--ALTER TABLE users
+--DROP CONSTRAINT UQ_users_username;
+
+
+
+
+
+-- ====== CHECK =======
+--CREATE TABLE products (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(100) NOT NULL,
+
+--	-- price decimal(10, 2) CHECK(price > 0)
+
+--	price decimal(10, 2),
+--	CONSTRAINT CK_products_price CHECK (price > 0)
+--);
+
+
+--CREATE TABLE products (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(100) NOT NULL,
+
+--	price decimal(10, 2) CHECK(price > 0),
+--	discount_price decimal(10, 2) CHECK (discount_price > 0),
+
+--	CONSTRAINT CK_products_price_discount_price CHECK (discount_price < price)
+--);
+
+
+--CREATE TABLE products (
+--	id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--	title varchar(100) NOT NULL,
+
+--	price decimal(10, 2) NOT NULL,
+--	discount_price decimal(10, 2) NOT NULL,
+
+--	CONSTRAINT CK_products_price
+--		CHECK (price > 0),
+
+--	CONSTRAINT CK_products_discount_price
+--		CHECK (discount_price > 0 AND discount_price < price)
+--);
+
+--ALTER TABLE products
+--ADD CONSTRAINT CK_products_title_length CHECK(LEN(title) > 10)
+
+
+
+
 
