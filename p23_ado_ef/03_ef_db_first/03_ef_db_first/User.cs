5871dd7e4d4aa5d125f23c5caf4071da64838ffe
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-12-08 02:06:04 +0300

    add: crud

--- p23_ado_ef/03_ef_db_first/03_ef_db_first/User.cs
+++ p23_ado_ef/03_ef_db_first/03_ef_db_first/User.cs
@@ -5,5 +5,5 @@ internal class User
     public int Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public int Age { get; set; }
+    public int? Age { get; set; }
 }
